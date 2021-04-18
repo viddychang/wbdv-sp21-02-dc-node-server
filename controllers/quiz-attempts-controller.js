8febe910612c987e6e60fa3b9e174713842c23c6
dchang11194 <dchang11194@gmail.com>
2021-04-18 00:14:21 -0400

    assignment8. heroku deploy

--- controllers/quiz-attempts-controller.js
+++ controllers/quiz-attempts-controller.js
@@ -1,6 +1,5 @@
 const quizAttemptDao = require('../daos/quiz-attempts-dao')
 
-
 module.exports = (app) => {
    app.post('/api/quizzes/:qid/attempts', (req, res) =>
        quizAttemptDao.createAttempt( req.params.qid, req.body)
